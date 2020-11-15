03fc8ab26b26880b290ddc8bc8fd00c6ce96b084
ovandermeer <owenvandermeer@gmail.com>
2020-11-15 16:43:56 -0500

    Commented out status changing command, will add to .ini later

--- cogs/userCommands.py
+++ cogs/userCommands.py
@@ -19,25 +19,25 @@ class userCommands(commands.Cog):
         print("Pong!")
         await ctx.send(f"Pong! Bot ping time: {round(self.client.latency * 1000)}ms")
 
-    @commands.command(aliases=["changestatus", "updatestatus", "status", "playing"])
-    async def changeStatus(self, ctx, *, statusInput=""):
-        if statusInput == "":
-            await ctx.send("Please enter a status for the bot")
-        else:
-            lowerStatusInput = statusInput.lower()
-            if lowerStatusInput.startswith("playing "):
-                statusOutput = statusInput.split(" ", 1)
-                await self.client.change_presence(status=discord.Status.online, activity=discord.Game(statusOutput[1]))
-                await ctx.send(
-                    f'Status updated to "Playing {statusOutput[1]}"! Please note this is not permenant, and will be reset when the bot is rebooted.')
-                currentDT = SupportingFunctions.getTime()
-                print(f"[{currentDT}] Status updated to playing '{statusOutput[1]}''")
-            else:
-                await self.client.change_presence(status=discord.Status.online, activity=discord.Game(statusInput))
-                await ctx.send(
-                    f'Status updated to "Playing {statusInput}"! Please note this is not permenant, and will be reset when the bot is rebooted.')
-                currentDT = SupportingFunctions.getTime()
-                print(f"[{currentDT}] Status updated to 'playing {statusInput}''")
+    # @commands.command(aliases=["changestatus", "updatestatus", "status", "playing"])
+    # async def changeStatus(self, ctx, *, statusInput=""):
+    #    if statusInput == "":
+    #        await ctx.send("Please enter a status for the bot")
+    #    else:
+    #        lowerStatusInput = statusInput.lower()
+    #        if lowerStatusInput.startswith("playing "):
+    #            statusOutput = statusInput.split(" ", 1)
+    #            await self.client.change_presence(status=discord.Status.online, activity=discord.Game(statusOutput[1]))
+    #            await ctx.send(
+    #                f'Status updated to "Playing {statusOutput[1]}"! Please note this is not permenant, and will be reset when the bot is rebooted.')
+    #            currentDT = SupportingFunctions.getTime()
+    #            print(f"[{currentDT}] Status updated to playing '{statusOutput[1]}''")
+    #        else:
+    #            await self.client.change_presence(status=discord.Status.online, activity=discord.Game(statusInput))
+    #            await ctx.send(
+    #                f'Status updated to "Playing {statusInput}"! Please note this is not permenant, and will be reset when the bot is rebooted.')
+    #            currentDT = SupportingFunctions.getTime()
+    #            print(f"[{currentDT}] Status updated to 'playing {statusInput}''")
 
     # @commands.command(aliases=["prefix", "changeprefix"])
     # async def changePrefix(self, ctx, newPrefix=""):
@@ -87,3 +87,4 @@ class userCommands(commands.Cog):
 
 def setup(client):
     client.add_cog(userCommands(client))
+
