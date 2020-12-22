da2091d35469b26bcf1d9f5c6ac0335f63353768
Owen VanderMeer <owenvandermeer@gmail.com>
2020-12-21 22:42:52 -0500

    possibly fixed again

--- cogs/funCommands.py
+++ cogs/funCommands.py
@@ -117,7 +117,7 @@ class funCommands(commands.Cog):
                 if quote.count('-') > 1:
                     quote = quote.split("-")
                 else:
-                    quote = quote.rsplit("-", 1)
+                    quote = quote.rsplit("-", 0)
                 fullQuote = quote[0]
                 quoteAuthor = quote[1]
             else:
